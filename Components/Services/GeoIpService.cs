184cbe02d79a7e212b1dca9b9aecaf003cae769c
kostyantyndenysov <kostiantyn.denysov@enpal.de>
2025-05-22 14:29:52 +0200

    fix(be): replaced ip data file location

--- Components/Services/GeoIpService.cs
+++ Components/Services/GeoIpService.cs
@@ -5,10 +5,12 @@ namespace UrlSamurai.Components.Services;
 
 public static class GeoIpService
 {
-    private static readonly string DbPath = Path.Combine(
-        Path.GetDirectoryName(typeof(GeoIpService).Assembly.Location)!,
-        "GeoLite2-Country.mmdb"
-    );
+    // private static readonly string DbPath = Path.Combine(
+    //     Path.GetDirectoryName(typeof(GeoIpService).Assembly.Location)!,
+    //     "GeoLite2-Country.mmdb"
+    // );
+
+    private const string DbPath = "GeoLite2-Country.mmdb";
 
     public static string? GetCountry(string? ip)
     {
